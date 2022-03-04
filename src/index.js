806b3ea03bedca5e3bad4e1455321a1a636c0794
MiaFate <miapurplefroot@gmail.com>
2022-03-04 15:05:51 -0300

    changes on index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import app from './app';
+//import app from './app';
+
+import express from "express";
+import morgan from "morgan";
+
+const routerApi = require("./routes/api");
+const app = express();
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api", routerApi);
+
 app.listen(process.env.PORT || 4000);
 console.log("server listening on port", 4000);
